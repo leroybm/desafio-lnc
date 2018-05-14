3da9921633a8efdee69e63795f943fb2b37c7ce7
Leroy Medeiros <leroymedeiros@pop-os.localdomain>
2018-05-14 20:59:08 -0300

    Enables grunt for watching scss/js and converting/minifying them

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sass: {
+            css: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/style.css': 'css/style.scss'
+                }
+            }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'js/script.min.js': 'js/script.js'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['**/*.scss'],
+                tasks: ['sass']
+            },
+            js: {
+                files: ['**/*.js'],
+                tasks: ['uglify']
+            }
+        }
+    });
+  
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.registerTask('default', ['watch']);
+
+};
