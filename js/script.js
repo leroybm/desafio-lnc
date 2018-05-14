10298fe20cb11ee9789e3d9f17a852c856fe31b9
Leroy Medeiros <leroymedeiros@pop-os.localdomain>
2018-05-14 20:33:44 -0300

    Encapsulates the script on a Module Pattern Normalizes the script structure (trailing space, double quotes)

--- js/script.js
+++ js/script.js
@@ -1,114 +1,97 @@
-const reference = document.querySelector('#reference-item'); 
-const carousel = document.querySelector('#recomendation-carousel');
-const next = document.querySelector('#recomendation-carousel-next');
-const prev = document.querySelector('#recomendation-carousel-prev');
+const Display = function() {
+    "use strict";
 
-(function() {
-    let script = document.createElement('script');
-    script.src = 'https://roberval.chaordicsystems.com/challenge/challenge.json?callback=X';
-    document.querySelector('body').appendChild(script);
-})();
+    const reference = document.querySelector("#reference-item");
+    const carousel = document.querySelector("#recomendation-carousel");
+    const next = document.querySelector("#recomendation-carousel-next");
+    const prev = document.querySelector("#recomendation-carousel-prev");
+
+    /**
+     * Transitions to the next element of the carousel
+     *
+     * @param {Event} event - The click event
+     */
+    function nextSlideHandler(event) {
+        event.preventDefault();
+        if (carousel.classList.contains("is-set") || carousel.classList.contains("is-reversing")) {
+            return;
+        }
+
+        function nextElement() {
+            carousel.classList.remove("is-set");
+            carousel.appendChild(carousel.firstElementChild);
+            carousel.removeEventListener("transitionend", nextElement);
+        }
+        carousel.addEventListener("transitionend", nextElement);
+        carousel.classList.add("is-set");
+    }
+
+    /**
+     * Transitions to the previous element of the carousel
+     *
+     * @param {Event} event - The click event
+     */
+    function prevSlideHandler(event) {
+        event.preventDefault();
+
+        // Prevents multiple animations running simultaneously
+        if (carousel.classList.contains("is-set") || carousel.classList.contains("is-reversing")) {
+            return;
+        }
+
+        carousel.insertBefore(carousel.lastElementChild, carousel.firstElementChild);
+
+        function prevElement() {
+            carousel.classList.remove("is-reversing");
+            carousel.removeEventListener("transitionend", prevElement);
+        }
+        carousel.addEventListener("transitionend", prevElement);
+        carousel.classList.add("is-reversing");
+    }
+
+    /**
+    * Returns a product as a string of valid html
+    *
+    * @param {Object} reference - The product object
+    * @param {String} styleClass - The root node html class
+    */
+    function populateProduct(product, styleClass) {
+        return `<a href="${product.detailUrl}" class="${styleClass}">
+                    <article>
+                        <figure>
+                            <img src="http:${product.imageName}" alt='${product.name}'>
+                        </figure>
+                        <h3>${product.name}</h3>
+                        <p>${product.oldPrice?"De "+product.oldPrice:""}</p>
+                        <p>Por: <strong>${product.price}</strong></p>
+                        <p>${product.productInfo.paymentConditions}</p>
+                    </article>
+                </a>`;
+    }
+
+    next.addEventListener("click", nextSlideHandler);
+    prev.addEventListener("click", prevSlideHandler);
+
+    let script = document.createElement("script");
+    script.src = "http://roberval.chaordicsystems.com/challenge/challenge.json?callback=X";
+    document.querySelector("body").appendChild(script);
+
+    return {
+        populateProduct: populateProduct
+    }
+}();
 
 /**
  * Handles the data from the remote service
- * 
- * @param {Object} response - The responce with the reference and recomendation data 
+ *
+ * @param {Object} response - The responce with the reference and recomendation data
  */
 function X(response) {
     const recommendation = response && response.data && response.data.recommendation;
-    
-    carousel.innerHTML = populateRecomendations(response.data.recommendation);
-    reference.innerHTML = populateReference(response.data.reference.item);
+
+    document.querySelector("#recomendation-carousel").innerHTML = recommendation.reduce((acc, product) => {
+        return acc += Display.populateProduct(product, "display-recomendations__item");
+    }, "");
+
+    document.querySelector("#reference-item").innerHTML = Display.populateProduct(response.data.reference.item, "display-reference__item");
 }
-
-/**
- * Transitions to the next element of the carousel
- *
- * @param {Event} event - The click event 
- */
-function nextSlideHandler(event) {
-    event.preventDefault();
-
-    // Prevents multiple animations running simultaneously    
-    if(carousel.classList.contains('is-set') || carousel.classList.contains('is-reversing')) {
-        return;
-    }
-
-    function nextElement() {
-        carousel.classList.remove('is-set');
-        carousel.appendChild(carousel.firstElementChild);
-        carousel.removeEventListener('transitionend', nextElement);
-    }
-
-    carousel.addEventListener('transitionend', nextElement);
-    carousel.classList.add('is-set');
-}
-
-/**
- * Transitions to the previous element of the carousel
- *
- * @param {Event} event - The click event 
- */
-function prevSlideHandler(event) {
-    event.preventDefault();
-
-    // Prevents multiple animations running simultaneously
-    if(carousel.classList.contains('is-set') || carousel.classList.contains('is-reversing')) {
-        return;
-    }
-
-    carousel.insertBefore(carousel.lastElementChild, carousel.firstElementChild);
-
-    function prevElement() {
-        carousel.classList.remove('is-reversing');
-        carousel.removeEventListener('transitionend', prevElement);  
-    }
-
-    carousel.addEventListener('transitionend', prevElement);
-    carousel.classList.add('is-reversing'); 
-}
-
-/**
-* Returns the reference product as a string of valid html
-*
-* @param {Object} reference - The reference product object
-*/
-function populateReference(reference) {
-    return(`
-        <a href="${reference.detailUrl}" class="display-reference__item">
-            <article>
-                <figure>
-                    <img src="${reference.imageName}" alt="">
-                </figure>
-                <h3>${reference.name}</h3>
-                <p>${reference.oldPrice ? 'De ' + reference.oldPrice : ''}</p>
-                <p>Por: <strong>${reference.price}</strong></p>
-                <p>${reference.productInfo.paymentConditions}</p>
-            </article>
-        </a>`);
-}
-
-/**
-* Returns a list of product recomendation items as a string of valid html
-*
-* @param {Object} recomendations - The list of product recomendations
-*/
-function populateRecomendations(recommendations) {
-    return recommendations.reduce((recommendationHtml, recommendation) => {
-        return recommendationHtml += `
-        <a href="${recommendation.detailUrl}" class="display-recomendations__item">
-            <article>
-                <figure>
-                    <img src="${recommendation.imageName}" alt="">
-                </figure>
-                <h3>${recommendation.name}</h3>
-                <p>${recommendation.oldPrice ? 'De ' + recommendation.oldPrice : ''}</p>
-                <p>Por: <strong>${recommendation.price}</strong></p>
-                <p>${recommendation.productInfo.paymentConditions}</p>
-            </article>
-        </a>`;
-    }, '');
-}
-
-next.addEventListener('click', nextSlideHandler);
-prev.addEventListener('click', prevSlideHandler);
