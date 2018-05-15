55a5b983a02e3ed64475af30c593ffa6c2793f5e
Leroy Medeiros <leroymedeiros@pop-os.localdomain>
2018-05-14 21:34:08 -0300

    Prevents grunt running on a loop to uglify recently uglifgied ugly js

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
                 tasks: ['sass']
             },
             js: {
-                files: ['**/*.js'],
+                files: ['js/script.js'],
                 tasks: ['uglify']
             }
         }
